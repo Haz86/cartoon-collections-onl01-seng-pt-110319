a74332ea09a059be4614b3fb35c55528a7af67c2
Hussain Ahmed <haz6798@icloud.com>
2019-12-22 04:39:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,14 @@ def roll_call_dwarves# code an argument here
 end
 
 def summon_captain_planet# code an argument here
+nu_array = []
+end	  i = 0 
 
+  while i < array.length
+def find_the_cheese# code an argument here	    nu_array << array[i].capitalize + "!"
+  # the array below is here to help	    i += 1 
+  cheese_types = ["cheddar", "gouda", "camembert"]	  end 
+end	  nu_array
 end
 
 def long_planeteer_calls# code an argument here
